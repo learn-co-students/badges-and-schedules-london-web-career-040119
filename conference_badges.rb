2e8350eee8e29991a5e504c147283c1d443f8f52
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-03-18 04:06:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,23 +2,19 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-public :badge_maker
-
 def batch_badge_creator(attendees)
-  attendees.collect { |name| name.badge_maker(name) }
+  attendees.collect { |name| badge_maker(name) }
 end
 
-public :batch_badge_creator
+
 
 def assign_rooms(attendees)
-  new_array = attendees.map.with_index { |name, i| i + 1 }
+  ##new_array = attendees.map.with_index { |name, i| i + 1 }
     attendees.map.with_index { |name, i|
-      "Hello, #{name}! You'll be assigned to room #{new_array[i]}!" }
+      "Hello, #{name}! You'll be assigned to room #{i + 1}!" }
     end
 
 def printer(attendees)
   batch_badge_creator(attendees).each { |name| puts name }
-  room_assignments(attendees).each { |i| puts i }
+  assign_rooms(attendees).each { |i| puts i }
 end
-
-public :printer
