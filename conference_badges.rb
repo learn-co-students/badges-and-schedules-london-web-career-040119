f639e8152757e44f3a23306dff753960b81bf78a
Ay Has <ahashim983@yahoo.com>
2019-03-31 18:38:44 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,20 +3,24 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
+  badges_format = Array.new
   for name in names
-    badge_maker(name)
+    badges_format.push(badge_maker(name))
   end
+  return badges_format
 end
 
 def assign_rooms(speakers)
   speaker_rooms = Array.new
-  speakers.each_with_index {|speaker,ind| speaker_rooms.push("Hello #{speaker}! You'll be assigned to room #{ind+1}")}
+  speakers.each_with_index {|speaker,ind| speaker_rooms.push("Hello, #{speaker}! You'll be assigned to room #{ind+1}!")}
   return speaker_rooms
 end
 
-def printer
-  batch_badge_creator
-  for item in assign_rooms
+def printer(speakers)
+  for item in batch_badge_creator(speakers)
+    puts item
+  end
+  for item in assign_rooms(speakers)
     puts item
   end
 end
